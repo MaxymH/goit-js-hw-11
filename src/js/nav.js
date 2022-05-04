6bfd9955cff642a36cbc902cdc69c45a11871b8a
Max_Hrytsenia <95626499+MaxymH@users.noreply.github.com>
2022-05-04 16:58:11 +0200

    sad

--- src/js/nav.js
+++
@@ -1,4 +0,0 @@
-const input = document.getElementById('search-box')
-const country = document.querySelector('.country-info');
-const countriesList = document.querySelector('.country-list');
-export{ input, countriesList, country}
